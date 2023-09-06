30af21f33b838fb902e1ea807307a580a7beebc8
Lucas Catto <lucascattosilva2018@gmail.com>
2023-09-05 17:30:19 -0700

    Much better now, menu and JS.

---
+++ _root/js/menu.js
@@ -0,0 +1,13 @@
+function menuShow() {
+    
+    let menuMobile = document.querySelector('.mobile-menu');
+
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "img/header/open.svg";
+    }
+    else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "img/header/close.svg";
+    }
+}
