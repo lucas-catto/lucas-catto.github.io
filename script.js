eb46b34218110dd089582f7276f81e81df8f94ba
lucas-catto <lucascattosilva2018@gmail.com>
2023-12-15 23:38:50 -0800

    Creating a new static version of my website.

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menuItens  = document.querySelector('.menu-itens');
-
-    menuToggle.addEventListener('click', function () {
-        menuItens.classList.toggle('active');
-    })
-})
